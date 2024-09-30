bfcf4fc45ffabdcf6a94a8746f72653dd15a6fff
Исламов Мурад Гюльмуса оглы <99683993+S1stemus@users.noreply.github.com>
2024-09-30 13:50:44 +0300

    add user_avatar

--- snp_site/models_app/models/user.py
+++ snp_site/models_app/models/user.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 class User(AbstractUser):
+    avatar = models.ImageField(upload_to='images/avatars', verbose_name="Аватар", null=True, blank=True)
     class Meta:
         db_table = 'users'
         verbose_name_plural = 'Пользователи'
         verbose_name = 'Пользователь'
+    
+    
