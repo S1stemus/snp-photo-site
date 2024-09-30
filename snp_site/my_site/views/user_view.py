a240701fee4c1d8487d1fde6b60e8da3606138a8
Исламов Мурад Гюльмуса оглы <99683993+S1stemus@users.noreply.github.com>
2024-09-30 13:51:36 +0300

    create  user page view

---
+++ snp_site/my_site/views/user_view.py
@@ -0,0 +1,26 @@
+from urllib import request
+from django.views.generic import DetailView
+from models_app.models import User
+from models_app.models.photo.fsm import State
+from django.db.models import Count
+class UserView(DetailView):
+    model = User
+    template_name = 'user_page.html'
+    slug_url_kwarg = 'username'
+    slug_field = 'username'
+    
+
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        filter=self.request.GET.get('filter')
+        photos=self.get_object().photos.filter(state=State.APPROVED,user=self.request.user.id)
+        if filter=='old':
+            photos=photos.order_by('created_at')
+        elif filter=='new':
+            photos=photos.order_by('-created_at')
+        elif filter=='popular':
+            photos=photos.annotate(like_count=Count('like')).order_by('-like_count')
+        context['photos']=photos
+        return context
+    
