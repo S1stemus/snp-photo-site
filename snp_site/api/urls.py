1d17d0bc7d36fae54304785f890128c100022cb3
Исламов Мурад Гюльмуса оглы <99683993+S1stemus@users.noreply.github.com>
2024-10-28 13:39:07 +0300

    add api views urls

--- snp_site/api/urls.py
+++ snp_site/api/urls.py
@@ -2,12 +2,18 @@ from api.views.photo import RetreivePhotoView
 from api.views.comment import RetreiveCommentView
 from django.urls import path
 from api.views.comment import RetreiveCommentView
-from api.services.like.create import CreateLikeService
 from api.views.like import LikePostView
+from api.views.comment import PostCommentView
+from api.views.photo import ListCreatePhotoView
+from api.views.user import *
 
 
 urlpatterns=[
     path('photo/<int:id>/', RetreivePhotoView.as_view()),
     path('comments/<int:id>/', RetreiveCommentView.as_view()),
-    path('like', LikePostView.as_view()),
+    path('comments/', PostCommentView.as_view()),
+    path('like/', LikePostView.as_view()),
+    path('photos/', ListCreatePhotoView.as_view()),
+    path('users/<int:id>/', UserShowView.as_view()),
+    path('users/photos/<int:id>/', ListUserPhotoView.as_view()),
 ]
