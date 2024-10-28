861cc28c18a383dfe08dd584d6a41011ce1c4292
Исламов Мурад Гюльмуса оглы <99683993+S1stemus@users.noreply.github.com>
2024-10-28 13:38:26 +0300

    add views

--- snp_site/api/views/like.py
+++ snp_site/api/views/like.py
@@ -1,11 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from service_objects.services import ServiceOutcome
-from api.serializers.like_serializer import LikeSerializer
+from api.serializers.likes.like_serializer import LikeSerializer
 from drf_spectacular.utils import extend_schema
 from api.services.like.create import CreateLikeService
 from rest_framework.permissions import IsAuthenticated
-from api.serializers.like_post_serializer import LikePostSerializer
+from api.serializers.likes.like_post_serializer import LikePostSerializer
+from rest_framework import status
 
 class LikePostView(APIView):
     permission_classes = [IsAuthenticated]
@@ -18,6 +19,6 @@ class LikePostView(APIView):
 
             )
     def post(self, request, *args, **kwargs):
-        outcome=ServiceOutcome(CreateLikeService,{'user':request.user.id,'photo':kwargs['photo_id']} )
-        return Response(LikeSerializer(outcome.result).data)
+        outcome=ServiceOutcome(CreateLikeService,{'current_user':request.user} )
+        return Response(LikeSerializer(outcome.result).data, status=status.HTTP_200_OK)
 
--- snp_site/api/views/photo.py
+++ snp_site/api/views/photo.py
@@ -2,17 +2,57 @@ from requests import delete
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from service_objects.services import ServiceOutcome
-from api.serializers.photo_serializer import PhotoSerializer
+from api.serializers.photos.photo_serializer import PhotoSerializer
+from api.serializers.photos.photo_post_serializer import PhotoPostSerializer
 from api.services.photo.show import ShowPhotoService
 from models_app.models.photo.models import Photo
 from drf_spectacular.utils import extend_schema
 from api.services.photo.delete import DeletePhotoService
+from api.services.photo.update import UpdatePhotoService
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
+from api.services.photo.list import ListPhotoService
+from api.services.photo.create import CreatePhotoService
+from rest_framework import status
+
+
+
+class ListCreatePhotoView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    @extend_schema(
+        tags=['Фотографии'],
+        summary='Создает фотографию',
+        description='Создает фотографию',
+        request=PhotoPostSerializer,
+        responses={
+            200: PhotoSerializer
+        }
+    )
+
+    def post(self, request, *args, **kwargs):
+        outcome=ServiceOutcome(CreatePhotoService, {'current_user':request.user}, request.FILES)
+        return Response(PhotoSerializer(outcome.result).data)
+    
+
+    permission_classes = [AllowAny]
+    @extend_schema(
+        tags=['Фотографии'],
+        summary='Возвращает все фотографии',
+        description='Возвращает все фотографии',
+        responses={
+            200: PhotoSerializer
+        }
+    )
+    def get(self, request, *args, **kwargs):
+        outcome=ServiceOutcome(ListPhotoService,{} )
+        return Response(PhotoSerializer(outcome.result,many=True).data)
 
 
 class RetreivePhotoView(APIView):
-
-    
-
+    permission_classes = [AllowAny]
+     
     @extend_schema(
         tags=['Фотографии'],
         summary='Возвращает фотографию по id',
@@ -20,12 +60,15 @@ class RetreivePhotoView(APIView):
         responses={
             200: PhotoSerializer
         }
-        
-    )
+       
+    )    
     def get(self, request, *args, **kwargs):
         outcome=ServiceOutcome(ShowPhotoService,{'id':kwargs['id']} )
         return Response(PhotoSerializer(outcome.result).data)
     
+   
+    
+    permission_classes = [IsAuthenticated]
     @extend_schema(
         tags=['Фотографии'],
         summary='Удаляет фотографию по id',
@@ -36,6 +79,21 @@ class RetreivePhotoView(APIView):
     )    
     def delete(self, request, *args, **kwargs):
         outcome=ServiceOutcome(DeletePhotoService,{'id':kwargs['id']} )
+        if outcome.result:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    @extend_schema(
+        tags=['Фотографии'],
+        summary='Обновляет фотографию по id',
+        description='Обновляет фотографию по id',
+        responses={
+            200: PhotoSerializer
+        },
+        request=PhotoPostSerializer
+    )    
+    def put(self, request, *args, **kwargs):
+        outcome=ServiceOutcome(UpdatePhotoService,{'id':kwargs['id'],'current_user':request.user} | request.data, request.FILES,)
         return Response(PhotoSerializer(outcome.result).data)
     
    
