6d1736b56380403e1ef0e9a50ff9eecf5453738e
S1stemus <islamovm988@gmail.com>
2024-10-29 18:19:26 +0300

    add sorting to photo_list method

--- snp_site/snp_site/settings/db_settings.py
+++ snp_site/snp_site/settings/db_settings.py
@@ -3,13 +3,13 @@ import environ
 env =environ.Env()
 environ.Env.read_env()
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':  env('DB_NAME', cast=str),
-        'USER': env('DB_USER', cast=str),
-        'PASSWORD': env('DB_PASSWORD', cast=str),
-        'HOST': 'localhost',
-        'PORT': 5432
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME':  env('DB_NAME', cast=str),
+#         'USER': env('DB_USER', cast=str),
+#         'PASSWORD': env('DB_PASSWORD', cast=str),
+#         'HOST': 'localhost',
+#         'PORT': 5432
+#     }
+# }
