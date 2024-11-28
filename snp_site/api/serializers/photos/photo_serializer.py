d74c978529de2f0dface2be65bd3d83db3047544
Исламов Мурад Гюльмуса оглы <99683993+S1stemus@users.noreply.github.com>
2024-11-28 11:22:29 +0300

    add user serializer to photo serializer

--- snp_site/api/serializers/photos/photo_serializer.py
+++ snp_site/api/serializers/photos/photo_serializer.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 from models_app.models import Photo
+from api.serializers.user.show import UserSerializer
 class PhotoSerializer(serializers.ModelSerializer):
     like_count = serializers.SerializerMethodField()
     comment_count = serializers.SerializerMethodField()
+    user=UserSerializer()
     class Meta:
         model = Photo
         fields = ['id','photo', 'description', 'user', 'created_at', 'updated_at','state','like_count','comment_count']
