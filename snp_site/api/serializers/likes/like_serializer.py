8f823374fef827f73cc68bc8b2b78c326c531278
Исламов Мурад Гюльмуса оглы <99683993+S1stemus@users.noreply.github.com>
2024-11-28 11:21:46 +0300

    add like count to like serializer

--- snp_site/api/serializers/likes/like_serializer.py
+++ snp_site/api/serializers/likes/like_serializer.py
@@ -2,8 +2,11 @@ from rest_framework import serializers
 from models_app.models import Like
 
 class LikeSerializer(serializers.ModelSerializer):
-   
+    like_count = serializers.SerializerMethodField()
+
+    def get_like_count(self, obj)->int:
+        return obj.likes.count()
     class Meta:
         model = Like
-        fields = ['photo_id', 'user_id', 'id']
+        fields = ['photo_id', 'user_id', 'id', 'like_count']
         
