857dfa02c62f62f9596a596f2f032105c13f4efa
Исламов Мурад Гюльмуса оглы <99683993+S1stemus@users.noreply.github.com>
2024-12-19 19:10:02 +0300

    Update like.py

--- snp_site/api/views/like.py
+++ snp_site/api/views/like.py
@@ -5,20 +5,25 @@ from api.serializers.likes.like_serializer import LikeSerializer
 from drf_spectacular.utils import extend_schema
 from api.services.like.create import CreateLikeService
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 from api.serializers.likes.post import LikePostSerializer
+
+
 from rest_framework import status
 
 class LikePostView(APIView):
-    permission_classes = [IsAuthenticated]
-    @extend_schema(
-            tags=['Лайки'], 
-            summary='Добавление лайка', 
-            description='Добавление лайка', 
-            request= LikePostSerializer,
-            responses={200: LikeSerializer}
+    permission_classes = [AllowAny]
 
-            )
+    @extend_schema(
+        tags=['Лайки'],
+        summary='создает лайк',
+        description='создает лайк',
+        request=LikePostSerializer,
+        responses={
+            200: LikeSerializer
+        },
+    )
     def post(self, request, *args, **kwargs):
-        outcome=ServiceOutcome(CreateLikeService,{'current_user':request.user} )
+        outcome=ServiceOutcome(CreateLikeService,{'current_user':request.user}|request.data )
         return Response(LikeSerializer(outcome.result).data, status=status.HTTP_200_OK)
 
