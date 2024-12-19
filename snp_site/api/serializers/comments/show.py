da9baa0018a2fe673191eb137adf6cd3704ebf31
Исламов Мурад Гюльмуса оглы <99683993+S1stemus@users.noreply.github.com>
2024-12-19 19:07:38 +0300

    chang commentshow serializer

--- snp_site/api/serializers/comments/show.py
+++ snp_site/api/serializers/comments/show.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from models_app.models import Comment
 from api.views import comment
+from api.serializers.user.show import UserSerializer
 
 
 class CommentShowSerializer(serializers.ModelSerializer):
+    user=UserSerializer()
     
     class Meta:
         model = Comment
