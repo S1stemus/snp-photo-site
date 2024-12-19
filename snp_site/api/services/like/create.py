94cc8cd1693089a5527f542b3c09a2f68dddfa8c
Исламов Мурад Гюльмуса оглы <99683993+S1stemus@users.noreply.github.com>
2024-12-19 19:09:44 +0300

    Update create.py

--- snp_site/api/services/like/create.py
+++ snp_site/api/services/like/create.py
@@ -1,3 +1,4 @@
+from ast import Not
 from re import M
 from service_objects.services import ServiceWithResult
 from django import forms
@@ -6,29 +7,36 @@ from service_objects.errors import NotFound
 from models_app.models.user import User
 from service_objects.fields import ModelField
 
+from models_app.models.photo.models import Photo
+
+
 class CreateLikeService(ServiceWithResult):
     photo_id=forms.IntegerField(required=True, min_value=1)
     current_user=ModelField(User)
     
     custom_validations = [
-        '_validate_like_id',
+        '_validate_photo_id',
         '_validate_user_id'
         ]
 
     def process(self):
         self.run_custom_validations()
+        if self.is_valid():
+            self.result = self._create_like()
+        return self
 
     def _create_like(self):
-        if Like.objects.filter(id=self.cleaned_data['photo_id'], user=self.cleaned_data['current_user']).exists():
-            Like.objects.delete(id=self.cleaned_data['photo_id'], user=self.cleaned_data['current_user'])
+        if Like.objects.filter(photo_id=self.cleaned_data['photo_id'], user=self.cleaned_data['current_user']).exists():
+           like = Like.objects.get(photo_id=self.cleaned_data['photo_id'], user=self.cleaned_data['current_user'])
+           like.delete()          
         else:
-            return Like.objects.create(id=self.cleaned_data['photo_id'], user=self.cleaned_data['current_user'])
+            return Like.objects.create(photo_id=self.cleaned_data['photo_id'], user=self.cleaned_data['current_user'])
             
         return None
-    def _validate_like_id(self):
-        if not Like.objects.filter(id=self.cleaned_data['photo_id']).exists():
-            self.add_error('id', 'Лайк не найден')
+    def _validate_photo_id(self):
+        if not Photo.objects.filter(id=self.cleaned_data['photo_id']).exists():
+            self.add_error('id',NotFound(message = f'Лайк с id {self.cleaned_data["photo_id"]} не найден '))
     def _validate_user_id(self):
         if not self.cleaned_data['current_user']:
-            self.add_error('current_user', 'Пользователь не найден')
+            self.add_error('current_user', NotFound(message=f'Пользователь не найден'))
     
