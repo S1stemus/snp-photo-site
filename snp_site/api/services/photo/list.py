3f1bb965af98ff96f78417968ba904225052e026
Исламов Мурад Гюльмуса оглы <99683993+S1stemus@users.noreply.github.com>
2025-01-13 08:48:11 +0300

    update sorting and filtering photos

--- snp_site/api/services/photo/list.py
+++ snp_site/api/services/photo/list.py
@@ -5,15 +5,28 @@ from models_app.models.photo.fsm import State
 from django.core.paginator import Paginator, EmptyPage
 from django.db.models import Count
 from django.conf import settings
+from django.db.models import Q
+from django.db import models
+
+
+
 
 
 class ListPhotoService(ServiceWithResult):
 
+    class SortFields(models.TextChoices):
+        CREATED_AT='created_at','По дате создания'
+        POPULARITY='popularity','По популярности'
+        COMMENT_COUNT='comment_count','По количеству комментариев'
+
     page = forms.IntegerField(min_value=1, required=False)
     per_page = forms.IntegerField(min_value=1, required=False)
 
-    sort_field=forms.ChoiceField(choices=(('created_at', 'created_at'),('popularity', 'popularity'),('comment_count', 'comment_count')), required=False)
+    sort_field=forms.ChoiceField(choices=SortFields, required=False)
     sort_direction=forms.ChoiceField(choices=(('asc', 'asc'), ('desc', 'desc')), required=False)
+
+    search_field=forms.CharField(required=False)
+    
 
 
     def process(self) -> "ServiceWithResult":      
@@ -37,24 +50,23 @@ class ListPhotoService(ServiceWithResult):
 
     @property
     def _filtered_photos(self):
-        photo = Photo.objects.filter(state=State.APPROVED)
-        if self.cleaned_data['sort_field'] == 'popularity':
-            if self.cleaned_data['sort_direction'] == 'asc':
-                photo = photo.annotate(like_count=Count('like')).order_by('like_count')
-            else:
-                photo = photo.annotate(like_count=Count('like')).order_by('-like_count')
-        elif self.cleaned_data['sort_field'] == 'created_at':
-            if self.cleaned_data['sort_direction'] == 'asc':
-                photo = photo.order_by('created_at')
-            else:
-                photo = photo.order_by('-created_at')
-        elif self.cleaned_data['sort_field'] == 'comment_count':
-            if self.cleaned_data['sort_direction'] == 'asc':
-                photo = photo.annotate(comment_count=Count('model_relation')).order_by('comment_count')
-            else:
-                photo = photo.annotate(comment_count=Count('model_relation')).order_by('-comment_count')
+        photos = Photo.objects.filter(state=State.APPROVED).annotate(comment_count=Count('model_relation')).annotate(like_count=Count('like'))
 
-        return photo
+        if self.cleaned_data.get('search_field'):
+            photos=photos.filter(
+                Q(name__icontains=self.cleaned_data['search_field'])|
+                Q(user__username__icontains=self.cleaned_data['search_field'])|
+                Q(description__icontains=self.cleaned_data['search_field'])
+                )
+        sorting='' if self.cleaned_data.get('sort_direction') == 'asc' else '-'
+        if self.cleaned_data.get('sort_field') == 'popularity':
+            photos = photos.order_by(f'{sorting}like_count')          
+        elif self.cleaned_data.get('sort_field') == 'created_at':
+            photos = photos.order_by(f'{sorting}created_at')
+        elif self.cleaned_data.get('sort_field') == 'comment_count':
+            photos = photos.order_by(f'{sorting}comment_count')
+        
+        return photos
         
 
     
--- snp_site/api/services/user/user_photo.py
+++ snp_site/api/services/user/user_photo.py
@@ -4,13 +4,37 @@ from models_app.models import User
 from service_objects.fields import ModelField
 from models_app.models.photo.models import Photo
 from models_app.models.photo.fsm import State
+from django.core.paginator import Paginator, EmptyPage
+from django.conf import settings
+from functools import lru_cache
+from django.db.models import Count
+from django.contrib.postgres.forms import SimpleArrayField
+from django.db import models
 
 
 
 
 class UserPhotoService(ServiceWithResult):
+    class SortFields(models.TextChoices):
+        CREATED_AT='created_at','По дате создания'
+        POPULARITY='popularity','По популярности'
+        COMMENT_COUNT='comment_count','По количеству комментариев'
+
+
     user_id = forms.IntegerField(min_value=1)
     current_user = ModelField(User,required=False)
+
+    page = forms.IntegerField(min_value=1, required=False)
+    per_page = forms.IntegerField(min_value=1, required=False)
+
+    sort_field=forms.ChoiceField(choices=SortFields, required=False)
+    sort_direction=forms.ChoiceField(choices=(('asc', 'asc'), ('desc', 'desc')), required=False)
+
+    state = SimpleArrayField(forms.ChoiceField(choices=State),required=False)
+
+    
+    
+    
 
     custom_validations = [
         '_validate_user'
@@ -23,12 +47,45 @@ class UserPhotoService(ServiceWithResult):
     def process(self):
         self.run_custom_validations()
         if self.is_valid():
-            self.result = self._user_photo
+            self.result = self._user_photos
         return self
+    
     @property
-    def _user_photo(self):
-        if User.objects.get(id=self.cleaned_data['user_id']) == self.cleaned_data['current_user']:           
-            return Photo.objects.filter(user=self.cleaned_data['user_id'])
-        else:
-            photos = Photo.objects.filter(user=self.cleaned_data['user_id'])
-            return photos.filter(state=State.APPROVED)
+    def _user_photos(self):
+        try:
+            return Paginator(
+                self._filtered_user_photos,
+                self.cleaned_data.get("per_page")
+                or settings.REST_FRAMEWORK["PAGE_SIZE"],
+            ).page(self.cleaned_data.get("page") or 1)
+        except EmptyPage:
+            return Paginator(
+                Photo.objects.none(),
+                self.cleaned_data.get("per_page")
+                or settings.REST_FRAMEWORK["PAGE_SIZE"],
+            ).page(1)
+
+    @property
+    def _filtered_user_photos(self):
+        photo=Photo.objects.filter(user=self.cleaned_data['user_id']).annotate(comment_count=Count('model_relation')).annotate(like_count=Count('like'))
+
+        sorting='' if self.cleaned_data.get('sort_direction') == 'asc' else '-'
+        if self.cleaned_data.get('sort_field') == 'popularity':
+            photo = photo.order_by(f'{sorting}like_count')          
+        elif self.cleaned_data.get('sort_field') == 'created_at':
+            photo = photo.order_by(f'{sorting}created_at')
+        elif self.cleaned_data.get('sort_field') == 'comment_count':
+            photo = photo.order_by(f'{sorting}comment_count')
+
+        if User.objects.get(id=self.cleaned_data['user_id']) != self.cleaned_data['current_user']:           
+            return photo.filter(state=State.APPROVED)
+        if self.cleaned_data.get('state'):
+            photo=photo.filter(state__in=self.cleaned_data['state'])
+            
+        return photo
+    
+    
+
+
+
+
