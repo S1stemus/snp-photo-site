87950eef979b796c80d191cdd9a3386b10684324
Исламов Мурад Гюльмуса оглы <99683993+S1stemus@users.noreply.github.com>
2025-01-13 08:47:42 +0300

    Update update.py

--- snp_site/api/services/photo/update.py
+++ snp_site/api/services/photo/update.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 from urllib import request
 from service_objects.services import ServiceWithResult
 from django import forms
@@ -5,12 +6,15 @@ from models_app.models import Photo
 from service_objects.errors import NotFound
 from models_app.models.user import User
 from service_objects.fields import ModelField
+from models_app.models.photo.fsm import State
+from models_app.models.photo.fsm import Flow
 
 
 class UpdatePhotoService(ServiceWithResult):
     id = forms.IntegerField(min_value=1)
     photo = forms.ImageField(required=False)
-    caption = forms.CharField(max_length=127, required=False)
+    description = forms.CharField(max_length=127, required=False)
+    name = forms.CharField(max_length=127, required=False)
     current_user = ModelField(User)
 
     
@@ -23,19 +27,33 @@ class UpdatePhotoService(ServiceWithResult):
     def process(self):
         self.run_custom_validations()
         if self.is_valid():
+            self._update_photo()
             self.result = self._photo
         return self
+    
     @property
+    @lru_cache
     def _photo(self):
-        try:
-            image = Photo.objects.get(id=self.cleaned_data['id'])
-            image.caption = self.cleaned_data['caption']
-            image.photo = self.cleaned_data['photo']
-            image.save()
-            return image
-        
+        try:                                        
+            return Photo.objects.get(id=self.cleaned_data['id'])        
         except Photo.DoesNotExist:
             return None
+    def _update_photo(self):       
+        description = self.cleaned_data['description']
+        photo = self.cleaned_data['photo']
+        name=self.cleaned_data['name']
+        prev_photo=self._photo.photo
+        prev_name=self._photo.name
+        prev_description=self._photo.description
+        self._photo.photo=photo
+        self._photo.name=name
+        self._photo.description=description
+        self._photo.prev_photo=prev_photo
+        self._photo.prev_name=prev_name
+        self._photo.prev_description=prev_description
+        self._photo.flow.update_to_waiting()
+
+
 
     def _validate_photo_id(self):
         if not self._photo:
