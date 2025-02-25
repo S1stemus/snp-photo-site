84df62881b7850509c4804e91bf5e42980f6c677
Исламов Мурад Гюльмуса оглы <99683993+S1stemus@users.noreply.github.com>
2025-02-25 11:23:58 +0300

    add update page

---
+++ snp_site/my_site/views/update_view.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView
+from models_app.models import Photo
+
+
+class UpdatePageView(CreateView, LoginRequiredMixin):
+       model = Photo
+       template_name = "update_page.html"
+       fields = ["photo", "description"]
+
+       def form_valid(self, form):
+            form.instance.user = self.request.user
+            return super().form_valid(form)
+
+       success_url = "/photos/"
+    
