d8864d0492bf089922421d020d34457d38460cd6
Исламов Мурад Гюльмуса оглы <99683993+S1stemus@users.noreply.github.com>
2025-02-25 11:25:05 +0300

    replace tasks.py

--- snp_site/snp_site/tasks.py
+++ snp_site/snp_site/tasks.py
@@ -5,13 +5,20 @@ from models_app.models import Photo
 from models_app.models.photo.fsm import State
 
 
+
 @shared_task
 def delete_photo(id):
     photo = Photo.objects.get(id=id)
     if photo.state in [State.ON_DELETE, State.REJECTED]:
-        photo.delete
+        photo.delete()
 
 
 @shared_task
 def print_word(word):
     print(word)
+
+@shared_task
+def myprint():
+    print('nice')
+
+
